5838bf303114f7837900d15dd61c4f1da25edb88
Divyanshi2806 <divyanshi2535@gmail.com>
2026-04-04 07:50:30 +0530

    Port

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app)
 const prisma = new PrismaClient()
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5000',
     methods: ['GET', 'POST'],
     credentials: true,
   }
